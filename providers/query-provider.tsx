28ff1940bc8795f651bec2bbf54fd21198b8a133
Ser Foll <sergiofollyvi@gmail.com>
2026-03-12 15:40:40 +0100

    added: tanstack query and query provider

---
+++ providers/query-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // ensure one QueryClient per instance
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: true,
+            retry: 1, // avoid aggressive retry loops
+          },
+        },
+      }),
+  );
+
+  return <>{children}</>;
+}
